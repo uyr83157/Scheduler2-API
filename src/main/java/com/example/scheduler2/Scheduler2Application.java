1a32d1b137b7e129ddbd88eba8f6bb0e1e77a761
uyr83157 <uyr83157@gmail.com>
2025-02-11 15:50:29 +0900

    Initial commit

---
+++ src/main/java/com/example/scheduler2/Scheduler2Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduler2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scheduler2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Scheduler2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduler2/Scheduler2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduler2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scheduler2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
