ffaa0e719b77caaa7ef53b04b7b583cac39c97f4
uyr83157 <uyr83157@gmail.com>
2025-02-13 00:21:58 +0900

    refactor: 구조 정리

--- src/main/java/com/example/scheduler2/exception/InvalidEmailFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.example.scheduler2.exception;
-
-// 이메일 형식 예외
-public class InvalidEmailFormatException extends RuntimeException {
-    public InvalidEmailFormatException(String message) {
-        super(message);
-    }
-}
